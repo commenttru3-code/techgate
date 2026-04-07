3e15bc981102aabf75b369e9be32ec11f35c2906
commenttru3-code <comment.tru3@gmail.com>
2026-04-07 16:01:52 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./techgate-slots.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
